7e70ea78c3434e5c04a8579fb088886ea411d361
Igor Petruk <ipetruk@google.com>
2023-11-29 22:00:02 +0000

    Added oatctl var rotate and moved var commands into subcommands

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,5 @@
 use anyhow::anyhow;
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 
 #[allow(unused)]
 mod ipc;
@@ -12,19 +12,83 @@ struct Cli {
     command: Commands,
 }
 
+#[derive(Clone, ValueEnum)]
+enum Direction {
+    Right,
+    Left,
+}
+
+#[derive(Subcommand)]
+enum VarSubcommand {
+    Set {
+        name: String,
+        value: String,
+    },
+    Get {
+        name: String,
+    },
+    Rotate {
+        name: String,
+        direction: Direction,
+        values: Vec<String>,
+    },
+}
+
 #[derive(Subcommand)]
 enum Commands {
     Poke,
-    SetVar { name: String, value: String },
-    GetVar { name: String },
+    Var {
+        #[clap(subcommand)]
+        var: VarSubcommand,
+    },
+}
+
+fn var_rotate(
+    name: String,
+    direction: Direction,
+    values: Vec<String>,
+) -> anyhow::Result<ipc::Response> {
+    if values.is_empty() {
+        return Err(anyhow::anyhow!("Values list must be not empty"));
+    }
+    let current_value = ipc::send_request(ipc::Request::GetVar { name: name.clone() })?;
+    let position = values
+        .iter()
+        .enumerate()
+        .find(|(_, v)| Some(*v) == current_value.value.as_ref())
+        .map(|(idx, _)| idx);
+    let last_idx = values.len() - 1;
+    use Direction::*;
+    let new_idx = match (direction, position) {
+        (Left, None) => last_idx,
+        (Left, Some(0)) => last_idx,
+        (Left, Some(l)) => l - 1,
+        (Right, None) => 0,
+        (Right, Some(x)) if x == last_idx => 0,
+        (Right, Some(l)) => l + 1,
+    };
+    let new_value = values.get(new_idx).expect("new_idx should be within range");
+    ipc::send_request(ipc::Request::SetVar {
+        name,
+        value: new_value.clone(),
+    })
 }
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     let response = match cli.command {
         Commands::Poke => ipc::send_request(ipc::Request::Poke),
-        Commands::SetVar { name, value } => ipc::send_request(ipc::Request::SetVar { name, value }),
-        Commands::GetVar { name } => ipc::send_request(ipc::Request::GetVar { name }),
+        Commands::Var { var } => match var {
+            VarSubcommand::Set { name, value } => {
+                ipc::send_request(ipc::Request::SetVar { name, value })
+            }
+            VarSubcommand::Get { name } => ipc::send_request(ipc::Request::GetVar { name }),
+            VarSubcommand::Rotate {
+                name,
+                direction,
+                values,
+            } => var_rotate(name, direction, values),
+        },
     }?;
     if let Some(error) = response.error {
         return Err(anyhow!("{}", error));
